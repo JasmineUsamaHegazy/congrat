e1cf14b549928681d7b4a0fd705d4dfcbca43393
JasmineUsamaHegazy <yuhijazy@gmail.com>
2022-09-19 23:39:16 +0300

    Q1 answer
    Signed-off-by: JasmineUsamaHegazy <yuhijazy@gmail.com>

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Welcome")
+d = input('name,age,address')
+d1 = input("name")
+d2 = input("age")
+d3 = input('address')
+
+
+print(type(d1))
+result = int(d2)
+print(type(d2))
+
+print("Hello Mr/Ms {name} age {age} located in {address}.thanks for being one of our community,   Enjoy")
+
+
+if d == "" or input is False:
+    print("Wrong Message")
+    print("stop program")
+
+
+
+
+
+
+
+
+
